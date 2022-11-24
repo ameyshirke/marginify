036586ae65a160c906c38b3d9f444219b99841bb
vishal1565 <vishal.1565@gmail.com>
2022-11-25 02:58:28 +0530

    Changes in UI Pages

--- src/app/pages/icons/icons.component.ts
+++ src/app/pages/icons/icons.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-icons",
-  templateUrl: "icons.component.html"
+  templateUrl: "icons.component.html",
+  styleUrls: ["icons.component.scss"]
 })
 export class IconsComponent implements OnInit {
   constructor() {}
